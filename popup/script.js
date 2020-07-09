1e970c60bb67d7efa8af4a75c56e20e41365dc57
pyk <x24870@gmail.com>
2020-07-09 19:49:10 +0800

    popup not finish yet

---
+++ popup/script.js
@@ -0,0 +1,8 @@
+
+const popupBtn = document.querySelector('#pop-btn');
+popupBtn.addEventListener('click', function(){
+  var popup = document.querySelector('#pop1');
+  popup.classList.toggle('show');
+});
+
+//refer: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_popup
