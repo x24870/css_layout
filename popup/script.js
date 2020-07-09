b7377e7ecd8a0849e2829d1cd28597dc9e73ea0a
pyk <x24870@gmail.com>
2020-07-09 22:46:01 +0800

    finish popup

--- popup/script.js
+++ popup/script.js
@@ -4,5 +4,3 @@ popupBtn.addEventListener('click', function(){
   var popup = document.querySelector('#pop1');
   popup.classList.toggle('show');
 });
-
-//refer: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_popup
